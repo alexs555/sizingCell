874f7c9d5ad9d6539cf49fbb81ea3bfd9617f29f
A.Shpirko <alexey@uniqsystems.ru>
2015-10-14 22:26:42 +0300

    Initial Commit

---
+++ testHeight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testHeight
+//
+//  Created by Алексей Шпирко on 14/10/15.
+//  Copyright (c) 2015 AlexShpirko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testHeight
+//
+//  Created by Алексей Шпирко on 14/10/15.
+//  Copyright (c) 2015 AlexShpirko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
